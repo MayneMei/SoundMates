e7cdadd075deaf91443736e6f014e7612ff0fec7
maynemei <maynemei@umich.edu>
2023-08-11 18:02:04 +0800

    modified spotify api

--- backend/src/routes/musicRoutes.js
+++ backend/src/routes/musicRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const MusicController = require("../controllers/MusicController");
+const MusicController = require("../controllers/musicController");
 
 const router = express.Router();
 
 // 添加音乐偏好
-router.post("/add-preference", MusicController.addPreference);
+// router.post("/add-preference", MusicController.addPreference);
 
 // 获取特定用户的音乐偏好
-router.get("/preferences/:userId", MusicController.getUserPreferences);
+// router.get("/preferences/:userId", MusicController.getUserPreferences);
 
 // 搜索音乐
 router.get("/search", MusicController.searchMusic);
