9cb32cb528e2b5408455ed848c474e480ca95a1e
Taxalfer <913576564@qq.com>
2023-08-11 16:03:56 +0800

    commit again

--- backend/src/routes/spotifyRoutes.js
+++ backend/src/routes/spotifyRoutes.js
@@ -7,7 +7,7 @@ const spotifyRouter = express.Router();
 // Spotify授权
 spotifyRouter.get("/authorize", (req, res) => {
   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
-  const scope = "user-read-private"; // 例如 'user-read-private user-read-email' ???这里暂时不知道是干嘛的
+  const scope = "user-read-private,playlist-read-private"; // 例如 'user-read-private user-read-email' ???这里暂时不知道是干嘛的
 
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
@@ -44,8 +44,6 @@ spotifyRouter.get("/callback", async (req, res) => {
     //TODO:保存accessToken，例如在session或其他地方
     const accessToken = response.data.access_token;
     req.session.spotifyToken = accessToken;
-    console.log("AccessToken:", accessToken);
-    console.log("Session Token after setting:", req.session.spotifyToken);
     //TODO: 确认合适的重定向url
     res.redirect("/thank-you-for-logging-in"); // 根据需要进行重定向
   } catch (error) {
